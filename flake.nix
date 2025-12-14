edd800e691d9632c77aa2eb1c9afa1213ec22b2b
Tomáš Čarnecký <tomc@caurea.org>
2025-12-14 15:55:46 +0100

    Initial import (#1)

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    nix-develop.url = "github:nicknovitski/nix-develop";
+    nix-develop.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      nix-develop,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+      in
+      {
+        packages.nix-develop = nix-develop.packages.${system}.default;
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.google-cloud-sdk
+            pkgs.nodejs
+            pkgs.pnpm
+            pkgs.biome
+          ];
+        };
+
+        devShells.workflow = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs
+            pkgs.pnpm
+            pkgs.biome
+          ];
+
+          shellHook = ''
+            pnpm install >/dev/null 2>&1
+          '';
+        };
+      }
+    );
+}
