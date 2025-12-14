81383e143a14c9b902fe08d5f966de53b086c7ff
Tomas Carnecky <tomc@caurea.org>
2025-12-14 23:49:58 +0100

    Add pinned-workflow-actions rule .

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,7 +1,13 @@
 import builtinCodeownersRule from "./builtin/codeowners.js";
+import builtinPinnedWorkflowActionsRule from "./builtin/pinned-workflow-actions.js";
 import builtinReadmeRule from "./builtin/readme.js";
 import builtinStaleBranchesRule from "./builtin/stale-branches.js";
 
-const builtinRules = [builtinCodeownersRule, builtinReadmeRule, builtinStaleBranchesRule];
+const builtinRules = [
+  builtinCodeownersRule,
+  builtinPinnedWorkflowActionsRule,
+  builtinReadmeRule,
+  builtinStaleBranchesRule,
+];
 
 export const allRules = [...builtinRules];
