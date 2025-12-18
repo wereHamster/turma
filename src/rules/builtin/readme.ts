d4814b620e04e386f1493f851428945886cde9d6
Tomas Carnecky <tomc@caurea.org>
2025-12-18 17:27:16 +0100

    . .

--- src/rules/builtin/readme.ts
+++ src/rules/builtin/readme.ts
@@ -7,6 +7,10 @@ const rule: Rule = {
     if (!(await fileExists(ctx, "README.md"))) {
       await addIssue(ctx, {
         title: "README.md does not exist",
+
+        description: `The repository does not contain a \`README.md\` file in its root directory.`,
+
+        remediation: `Create a \`README.md\` file in the root directory of the repository to provide an overview of the project, instructions for setup and usage, and any other relevant information for users and contributors.`,
       });
     }
   },
