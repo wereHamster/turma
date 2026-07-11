a1bf75e7a100e41f3f8dcf55a7307eed288b92eb
Tomáš Čarnecký <tomc@caurea.org>
2026-07-11 13:59:50 +0200

    Stop using flake-utils (#110)

--- flake.nix
+++ flake.nix
@@ -1,47 +1,39 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
-    flake-utils.url = "github:numtide/flake-utils";
+    systems.url = "github:nix-systems/default";
 
     nix-develop.url = "github:nicknovitski/nix-develop";
     nix-develop.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs =
-    {
-      nixpkgs,
-      flake-utils,
-      nix-develop,
-      ...
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+  outputs = { nixpkgs, systems, nix-develop, ... }: let
+    forAllSystems = function:
+      nixpkgs.lib.genAttrs (import systems) (
+        system: function nixpkgs.legacyPackages.${system}
+      );
+  in {
+    packages = forAllSystems (pkgs: {
+      nix-develop = nix-develop.packages.${pkgs.stdenv.system}.default;
+    });
 
-      in
-      {
-        packages.nix-develop = nix-develop.packages.${system}.default;
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShell {
+        nativeBuildInputs = [
+          pkgs.google-cloud-sdk
+          pkgs.nodejs
+          pkgs.pnpm
+          pkgs.biome
+        ];
+      };
 
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = [
-            pkgs.google-cloud-sdk
-            pkgs.nodejs
-            pkgs.pnpm
-            pkgs.biome
-          ];
-        };
-
-        devShells.workflow = pkgs.mkShell {
-          nativeBuildInputs = [
-            pkgs.nodejs
-            pkgs.pnpm
-            pkgs.biome
-          ];
-        };
-      }
-    );
+      workflow = pkgs.mkShell {
+        nativeBuildInputs = [
+          pkgs.nodejs
+          pkgs.pnpm
+          pkgs.biome
+        ];
+      };
+    });
+  };
 }
